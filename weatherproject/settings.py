4cf9be68a8525c5f2d43fce30297f0c5f825bf15
Syed Ali <syedali3267@gmail.com>
2021-03-17 20:29:19 +0000

    Added '.get'

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -10,13 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['WEATHERSECRET_KEY']
+SECRET_KEY = os.environ.get('WEATHERSECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 
 # Application definition
