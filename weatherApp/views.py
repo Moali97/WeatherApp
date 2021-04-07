54ae748e16a9987b7edf23a359ede3e81f23a901
Syed Ali <syedali3267@gmail.com>
2021-04-07 19:59:12 +0100

    Adding the ability to add multiple weather boxes

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -6,26 +6,31 @@ from .models import City
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}' \
-          '&units=metric&lang=lang&appid=5b96a99b221f2256b435467d7f5371f3 '
+          '&units=metric&lang=lang&appid=5b96a99b221f2256b435467d7f5371f3'
 
-    cities = City.objects.all()
+
 
     if request.method == 'POST':
         form = CityForm(request.POST)
         form.save()
 
     form = CityForm()
+
+    cities = City.objects.all()
+
     weather_data = []
 
     for city in cities:
+
         city_weather = requests.get(url.format(city.name)).json()
 
         weather = {
             'city': city.name,
             'temperature': city_weather['main']['temp'],
             'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon']
+            'icon': city_weather['weather'][0]['icon'],
         }
+
         weather_data.append(weather)
 
     context = {'weather_data': weather_data, 'form': form}
