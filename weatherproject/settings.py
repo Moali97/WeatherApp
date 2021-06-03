05b5584887ce6b0cff9762848a89fc074034b1ec
Syed Ali <syedali3267@gmail.com>
2021-06-03 18:38:00 +0100

    Commented out staticfiles storage

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # HTTPS settings
 SESSION_COOKIE_SECURE = True
